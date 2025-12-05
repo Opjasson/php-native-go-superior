36fc2865c488c1fd804bba5066395cc98c9779c5
Opjasson <azmitheusos333@gmail.com>
2025-12-05 21:34:09 +0700

    final : create testing final keyword

---
+++ finalKeyword.php
@@ -0,0 +1,14 @@
+<?php
+    final class Fruit {
+        public $name;
+
+        public function jenis() {
+          return "Buah {$this->name} adalah jenis buah buahan";  
+        }
+    }
+
+    // error code because extend from final class
+    class Inherit extends Fruit {
+
+    }
+?>

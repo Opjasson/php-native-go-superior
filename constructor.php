258b67631c57091d999cf2923f0ae8996aa7a561
Opjasson <azmitheusos333@gmail.com>
2025-12-05 14:02:06 +0700

    constructor : create multiple contructor property

--- constructor.php
+++ constructor.php
@@ -9,11 +9,40 @@ class Vegetable
         $this->brokoli = $name;
     }
 
-    function get_name() {
+    function get_name()
+    {
         return $this->brokoli;
     }
 }
 
-$brokoli = new Vegetable("Brokoli");
+$brokoli = new Vegetable("Brokoli<br>");
 
 echo $brokoli->get_name();
+
+class VegetableMultiple
+{
+    public $name;
+    public $color;
+
+    function __construct($name, $color)
+    {
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+    function get_name()
+    {
+        return $this->name;
+    }
+
+    function get_color() {
+        return $this->color;
+    }
+}
+
+$tomato = new VegetableMultiple("name : tomato<br>", "color : red");
+
+echo "------Constructor Multiple------ <br>";
+echo $tomato->get_name();
+echo $tomato->get_color();
+

6ed4f570ca55f1eee95abfd8f50e379b52be4531
Opjasson <azmitheusos333@gmail.com>
2025-12-12 07:57:27 +0700

    staticMethod : create xample static method

---
+++ staticMethod.php
@@ -0,0 +1,17 @@
+<?php 
+    class Classname {
+        public static function staticMethod() {
+            echo "Hello world";
+        }
+    }
+
+    Classname::staticMethod();
+
+    class greeting{
+        public static function welcome() {
+            echo "Hello world";
+        }
+    }
+
+    greeting::welcome();
+?>

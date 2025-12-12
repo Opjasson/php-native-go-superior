d3bb522105de2bb531fc31ba08b876cee006c038
Opjasson <azmitheusos333@gmail.com>
2025-12-12 14:20:08 +0700

    iterableImplement : create implement object

---
+++ iterableImplements.php
@@ -0,0 +1,50 @@
+<?php
+
+class Myiterator implements Iterator
+{
+    private $items = [];
+    private $pointer = 0;
+
+    public function __construct($items)
+    {
+        // array_values() makes sure that the keys are numbers
+        $this->items = array_values($items);
+    }
+
+    public function current(): mixed
+    {
+        return $this->items[$this->pointer];
+    }
+
+    public function key(): mixed
+    {
+        return $this->pointer;
+    }
+
+    public function next(): void
+    {
+        $this->pointer++;
+    }
+
+    public function rewind(): void
+    {
+        $this->pointer = 0;
+    }
+
+    public function valid(): bool
+    {
+        return $this->pointer < count($this->items);
+    }
+
+    // A function that uses iterables
+    function printIterable(iterable $myIterable)
+    {
+        foreach ($myIterable as $item) {
+            echo $item;
+        }
+    }
+}
+
+// Use the iterator as an iterable
+$iterator = new MyIterator(["a", "b", "c"]);
+printIterable($iterator);
